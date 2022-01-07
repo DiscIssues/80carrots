8186239c8c8a7c9c01c42fc2baf562b95f7e427e
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-01-07 15:45:37 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -6,8 +6,6 @@ if TBLFIND = ID then
 game.Players.PlayedAdded:Connect(function(player)
     if TBLFIND(Username,player.Name) then 
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/DiscIssues/80carrots/main/Main.lua"))()
-           else
-            game.Players.LocalPlayer:Kick("\nYou've been banned by: RAC\nFor the reason of: No\n999999999999 days remaining until unban")
         end
     end)
 
