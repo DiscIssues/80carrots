ea4d290a5037052464467afae216494683ef6301
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-01-07 15:15:38 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,10 +1,12 @@
 local TBLFIND = table.find
 
-local whitelistUserIDs = {}
+local whitelistUserIDs = {3031289837}
 
 if TBLFIND = ID then
 game.Players.PlayedAdded:Connect(function(player)
-    if TBLFIND(whitelistUserIDs,player.userID) then return end
+    if TBLFIND(whitelistUserIDs,player.userID) then 
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/DiscIssues/80carrots/main/Main.lua"))()
+                end
            else
             game.Players.LocalPlayer:Kick("\nYou've been banned by: RAC\nFor the reason of: No\n999999999999 days remaining until unban")
         end
