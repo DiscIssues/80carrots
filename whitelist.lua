e3ea6ba3dd47f648224c26865fab8e3002cd09a2
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-01-07 14:27:24 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -3,7 +3,7 @@ local TBLFIND = table.find
 local whitelistUserIDs = {}
 
 game.Players.PlayedAdded:Connect(function(player)
-    if not table.find(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
+    if not TBLFIND(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
         game.Players.LocalPlayer:Kick("\nYou've been banned by: RAC\nFor the reason of: No\n999999999999 days remaining until unban")
     end
 end)
