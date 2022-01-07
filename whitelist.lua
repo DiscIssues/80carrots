32a024a475f5d7f5942786a63117ea2e791c2ea3
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-01-07 14:26:05 +0800

    Update and rename README.md to whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,13 @@
+print"checking whitelist.."
+
+local TBLFIND = table.find
+
+local whitelistUserIDs = {}
+
+game.Players.PlayedAdded:Connect(function(player)
+    if not table.find(whitelistUserIDs,player.UserId) then --If the UserID value is not in the table this returns nil. In Lua nil equals false.
+        game.Players.LocalPlayer:Kick("\nYou've been banned by: RAC\nFor the reason of: No\n999999999999 days remaining until unban")
+    end
+end)
+
+wait"5"
