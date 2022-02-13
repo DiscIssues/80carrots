40d5a01036f949ca3e54a4e15a3ef616a095d4f3
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-02-13 11:07:32 +0800

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,7 +1,6 @@
-userids = {
-[0x9z0m3,0x1m9ak] = true,
-[0x9z0m3] = true,
-[0x1m9ak] = true
+keys = {
+[asdasd123124565464] = true,
+[friedchicken] = true,
+[discnigga] = true
 }
-return userids
-
+return keys
