7101483ea1bf0cb465407fcd5d4fd820fcc3e8e4
DiscIssues <97275202+DiscIssues@users.noreply.github.com>
2022-02-17 10:45:47 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,11 +1,2 @@
-local TBLFIND = table.find
-
-local Username = {HelloThereMyKidz,HeroXX002}
-
-if TBLFIND = ID then
-game.Players.PlayedAdded:Connect(function(player)
-    if TBLFIND(Username,player.Name) then 
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/DiscIssues/80carrots/main/Main.lua"))()
-        end
-    end)
+print"i suck dccoks"
 
